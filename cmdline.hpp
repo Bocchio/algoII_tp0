2c75d4800cdbe4e042b795332adec66a943b476b
Bocchio <guido@bocch.io>
2018-04-07 14:04:14 -0300

    Indenté un poco el código de cmdline (y le cambié las extensiones)
    Para que cumpla un poco más con la guía de estilo de Google. Sigue habiendo caracteres UTF-8 por todos lados, y eso está mal. Pero no es el momento para editar todos los comentarios.

--- cmdline.hpp
+++ cmdline.hpp
@@ -1,5 +1,5 @@
-#ifndef _CMDLINE_H_INCLUDED_
-#define _CMDLINE_H_INCLUDED_
+#ifndef CMDLINE_HPP_
+#define CMDLINE_HPP_
 
 #include <string>
 #include <iostream>
@@ -9,31 +9,31 @@
 #define OPT_MANDATORY 2
 
 struct option_t {
-	int has_arg;
-	const char *short_name;
-	const char *long_name;
-	const char *def_value;
-	void (*parse)(std::string const &);
-	int flags;
+    int has_arg;
+    const char *short_name;
+    const char *long_name;
+    const char *def_value;
+    void (*parse)(std::string const &);
+    int flags;
 };
 
 class cmdline {
-	// Este atributo apunta a la tabla que describe todas
-	// las opciones a procesar. Por el momento, sólo puede
-	// ser modificado mediante contructor, y debe finalizar
-	// con un elemento nulo.
-	//
-	option_t *option_table;
+    // Este atributo apunta a la tabla que describe todas
+    // las opciones a procesar. Por el momento, sólo puede
+    // ser modificado mediante contructor, y debe finalizar
+    // con un elemento nulo.
+    //
+    option_t *option_table;
 
-	// El constructor por defecto cmdline::cmdline(), es
-	// privado, para evitar construir parsers sin opciones.
-	//
-	cmdline();
-	int do_long_opt(const char *, const char *);
-	int do_short_opt(const char *, const char *);
-public:
-	cmdline(option_t *);
-	void parse(int, char * const []);
+    // El constructor por defecto cmdline::cmdline(), es
+    // privado, para evitar construir parsers sin opciones.
+    //
+    cmdline();
+    int do_long_opt(const char *, const char *);
+    int do_short_opt(const char *, const char *);
+ public:
+    explicit cmdline(option_t *);
+    void parse(int, char * const[]);
 };
 
-#endif
+#endif  // CMDLINE_HPP_

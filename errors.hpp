fee64796556c1564b555cd8a6ec6c8828556cdba
yagosalvador <37965771+yagosalvador@users.noreply.github.com>
2018-04-19 13:15:30 -0300

    Add files via upload

---
+++ errors.hpp
@@ -0,0 +1,13 @@
+#ifndef ERRORS_HPP__
+#define ERRORS_HPP__
+
+#define ERROR_MSJ_CANT_OPEN_FILE "Cannot open "
+#define ERROR_MSJ_UNKNOWN_METHOD "Unknown method."
+#define ERROR_MSJ_CORRUPTED_DATA "Input data was corrupt."
+#define ERROR_MSJ_INVALID_ARG "Invalid non-option argument: "
+#define ERROR_MSJ_OPT_REQ_ARG "Option requires argument: "
+#define ERROR_MSJ_UNKNOWN_OPT "Unknown option: "
+#define ERROR_MSJ_MANDATORY_OPT "Option is mandatory."
+#define HELP_MSJ "cmdline [-i file] [-o file] [-m method]"
+
+#endif
